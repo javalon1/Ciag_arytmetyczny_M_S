8861a1267a14def193ac2213d1c282b7ea0a2439
Michal Stusik <x5htpx@gmail.com>
2019-05-18 12:06:43 +0100

    2

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,30 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
+ ai = a1 + (i-1)* d
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj n: ");
+        int n = scanner.nextInt();
+        System.out.println("Podaj a1: ");
+        int a1 = scanner.nextInt();
+        System.out.println("Podaj d: ");
+        int d = scanner.nextInt();
+
+        int ciag = 0;
+
+        for (int i =1; i<=n; i++){
+            ciag = a1 + (i-1)*d;
+            System.out.println(ciag);
+        }
+
+
     }
 }
